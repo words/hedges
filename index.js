a3f3d83c64688d8cd712674714e385bb6669c2d4
wooorm <tituswormer@gmail.com>
2014-09-01 18:07:39 +0200

    Depend on datalist-interface instead of custom interface

--- index.js
+++ index.js
@@ -1,35 +1,8 @@
 'use strict';
 
-var words = require('./data/hedges.json');
+var words, Interface;
 
-function add() {
-    words.push.apply(words, arguments);
-}
+words = require('./data/hedges.json');
+Interface = require('datalist-interface');
 
-function remove() {
-    var iterator = -1,
-        length = arguments.length,
-        index, word;
-
-    while (++iterator < length) {
-        word = arguments[iterator];
-        index = words.indexOf(word);
-
-        if (index !== -1) {
-            words.splice(index, 1);
-        }
-    }
-}
-
-function is(word) {
-    return words.indexOf(word) !== -1;
-}
-
-function all() {
-    return words.concat();
-}
-
-exports.add = add;
-exports.remove = remove;
-exports.is = is;
-exports.all = all;
+module.exports = new Interface(words);
