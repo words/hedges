777093cc6e47d9efc71592c37e99fbab75b36908
Titus Wormer <tituswormer@gmail.com>
2016-11-06 21:09:08 +0100

    Flatten `script/`

--- build.js
+++ build.js
@@ -10,14 +10,13 @@
 
 /* Dependencies. */
 var fs = require('fs');
-var path = require('path');
 var toJSON = require('plain-text-data-to-json');
 
 /* Read. */
-var doc = fs.readFileSync(path.join(__dirname, '..', 'data.txt'), 'utf8');
+var doc = fs.readFileSync('data.txt', 'utf8');
 
 /* Transform. */
 var data = JSON.stringify(toJSON(doc), null, 2) + '\n';
 
 /* Write. */
-fs.writeFileSync(path.join(__dirname, '..', 'index.json'), data);
+fs.writeFileSync('index.json', data);
