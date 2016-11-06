5c512479eb0ee48255e02e89b8897472b0705bb2
Titus Wormer <tituswormer@gmail.com>
2016-11-06 21:11:42 +0100

    Remove superfluous comments

--- build.js
+++ build.js
@@ -1,22 +1,10 @@
-/**
- * @author Titus Wormer
- * @copyright 2014 Titus Wormer
- * @license MIT
- * @module hedges:script
- * @fileoverview Generate a dictionary of entity names to replacements.
- */
-
 'use strict';
 
-/* Dependencies. */
 var fs = require('fs');
 var toJSON = require('plain-text-data-to-json');
 
-/* Read. */
 var doc = fs.readFileSync('data.txt', 'utf8');
 
-/* Transform. */
 var data = JSON.stringify(toJSON(doc), null, 2) + '\n';
 
-/* Write. */
 fs.writeFileSync('index.json', data);
--- test.js
+++ test.js
@@ -1,19 +1,9 @@
-/**
- * @author Titus Wormer
- * @copyright 2014 Titus Wormer
- * @license MIT
- * @module hedges
- * @fileoverview Test suite for `hedges`.
- */
-
 'use strict';
 
-/* Dependencies. */
 var test = require('tape');
 var hedges = require('./');
 
-/* Tests. */
-test('characterEntities', function (t) {
+test('hedges', function (t) {
   t.equal(typeof hedges, 'object', 'should be an array #1');
   t.equal(Array.isArray(hedges), true, 'should be an array #2');
   t.notEqual(hedges.indexOf('appear'), -1, 'should contain words');
