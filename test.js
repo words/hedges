8a7a665c0944f29f03a9398e55d274b592e9ca05
Titus Wormer <tituswormer@gmail.com>
2021-04-05 17:59:54 +0200

    Update dev-dependencies

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 var test = require('tape')
 var hedges = require('.')
 
-test('hedges', function(t) {
+test('hedges', function (t) {
   t.equal(typeof hedges, 'object', 'should be an array #1')
   t.equal(Array.isArray(hedges), true, 'should be an array #2')
   t.notEqual(hedges.indexOf('appear'), -1, 'should contain words')
