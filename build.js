dab80cd582328cc21a11847a6ba4cfbe043add0e
Titus Wormer <tituswormer@gmail.com>
2022-11-06 13:09:54 +0100

    Refactor code-style

--- build.js
+++ build.js
@@ -1,9 +1,9 @@
 import fs from 'node:fs'
 import {toJson} from 'plain-text-data-to-json'
 
-var doc = fs.readFileSync('data.txt', 'utf8')
+const doc = fs.readFileSync('data.txt', 'utf8')
 
 fs.writeFileSync(
   'index.js',
-  'export var hedges = ' + JSON.stringify(toJson(doc), null, 2) + '\n'
+  'export const hedges = ' + JSON.stringify(toJson(doc), null, 2) + '\n'
 )
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export var hedges = [
+export const hedges = [
   'a bit',
   'about',
   'actually',
